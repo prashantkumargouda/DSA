8db3893dfde5f029fcce62e191a39ebb7e509a11
Prashant <prashantkumargouda17@gmail.com>
2025-11-05 22:31:48 +0530

    solved one more

---
+++ leetcode/17.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string> 
+#include <vector>
+
+using namespace std ; 
+
+// making use of backtracking recursion and mapping to solve the required problem 
+
+void solve(string digits , int i , string output , vector<string> &ans , string mapping[] ) { 
+    if( i>= digits.length() ){ 
+        ans.push_back(output); 
+        return ; 
+    } 
+
+    string letters = mapping[digits[i] - '0'] ; 
+
+    for(int j=0 ; j<letters.length() ; j++) { 
+        output.push_back(letters[j]) ; 
+        solve(digits , i+1 , output , ans , mapping ) ;
+        output.pop_back() ; 
+    } 
+} 
+
+int main() { 
+    string digits ; 
+    cout << "Enter the digits :" << endl; 
+
+    getline(cin , digits) ;  
+
+    vector<string> ans ;
+    string output ; 
+    // now mapping the elements 
+
+    string mapping[10] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" }; 
+
+    solve(digits , 0 , output , ans , mapping ) ; 
+
+    for( auto &sol : ans ){ 
+        cout << sol << " " ; 
+    } 
+    cout << endl; 
+
+    return 0 ; 
+
+}
