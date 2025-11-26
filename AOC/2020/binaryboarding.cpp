4467cd0a4f45b6d124b3c863db08f752a4f02547
Prashant <prashantkumargouda17@gmail.com>
2025-11-26 21:03:42 +0530

    solved one more

---
+++ AOC/2020/binaryboarding.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string> 
+#include <sstream>
+#include <vector> 
+#include <climits> 
+
+using namespace std ; 
+
+int main() {  
+
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);  
+
+    string line ;  
+    vector<string> codes ;  
+
+    while(getline(cin , line)) { 
+        codes.push_back(line) ; 
+    }    
+
+    int maxSeat = INT_MIN ;  
+
+    for( const string &code : codes ) { 
+        // extracting the row and col for the seats  
+        int col = 0 , row = 0 ;  
+
+        for(int i=0 ; i<7 ; i++) {
+            row = row << 1 ; 
+            if( code[i] == 'B' ) row |= 1 ; 
+        } 
+
+        for(int i=7 ; i<10 ; i++) { 
+            col = col << 1 ; 
+            if( code[i] == 'R' ) col |= 1 ; 
+        } 
+
+        int seatId = row * 8 + col ; 
+        if( maxSeat < seatId ) maxSeat = seatId ;  
+
+    }  
+    cout << maxSeat << endl; 
+
+    return 0 ; 
+}
