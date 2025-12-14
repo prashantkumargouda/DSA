70aaf967f847204409af82d252fd00a692599cb8
Prashant <prashantkumargouda17@gmail.com>
2025-12-14 13:08:41 +0530

    added some more

---
+++ practice/arrays/maxOnes.cpp
@@ -0,0 +1,61 @@
+// Maximum Consecutive Ones After Flipping Zeroes 
+
+#include <iostream>
+#include <vector> 
+#include <climits> 
+
+using namespace std ; 
+
+int maxOnes(vector<int> &arr , int k ) {
+    // explorint all the possible subarrays 
+    int n = arr.size() ; 
+    int res = INT_MIN ; 
+    
+    for(int i=0 ; i<n ; i++) {
+        int count = 0 ; // for each new subarray make the count zero 
+        for(int j=i ; j<n ; j++) {
+            if( arr[j] == 0 ) {
+                count++ ; 
+            } 
+
+            if( count <= k ) {
+                res = max(res , j-i+1 ) ;
+            }
+        }
+    } 
+
+    return res ; 
+} 
+
+int maxOnes2(vector<int> &arr , int k ) {
+    int n = arr.size() ; 
+    int res = INT_MIN ; 
+    int start = 0 , end = 0 ; 
+    int count = 0 ; 
+    while( end < arr.size() ){
+        if( arr[end] == 0 ) {
+            count++ ;
+        } 
+
+        if( count > k ) {
+            if( arr[start] == 0 ) { 
+                count-- ; 
+            } 
+            start++; 
+        } 
+
+        res = max( res , end - start + 1 ) ;
+
+        end++ ;  
+    } 
+
+    return res ;
+}
+
+int main() {
+    vector<int> arr= {1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1}; 
+    cout << maxOnes(arr , 2) << endl; 
+    cout << maxOnes2(arr , 2) << endl; 
+
+    return 0 ; 
+}
--- practice/arrays/twoSum.cpp
+++ practice/arrays/twoSum.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector> 
+#include <algorithm> 
+#include <unordered_set> 
 
 using namespace std ; 
 
@@ -14,11 +16,103 @@ int two(vector<int> &arr , int target ) {
     return false ; 
 } 
 
+bool binarySearch(vector<int> &arr ,int start , int end , int target ) {
+    while(start <= end) { 
+        int mid = start + (end - start) / 2 ; 
+        if( target > arr[mid] ) {
+            start = mid + 1 ; 
+        } 
+        else if( target < arr[mid] ) {
+            end = mid -1 ; 
+        }else{
+            return true ;
+        }
+    } 
+    return false ; 
+} 
+
+bool twoSum2(vector<int> &arr , int target ) {
+    int n = arr.size() ; 
+
+    // using the binary search method  
+    // it makes use of a complement along with binary search to find if it exists 
+    sort(arr.begin() , arr.end()) ; 
+
+    for(int i=0 ; i<n ; i++) {
+        int comp = target - arr[i] ; 
+
+        if( binarySearch(arr , i+1 , arr.size() - 1 , comp )) {
+            return true ; 
+        }
+    } 
+
+    return false ; 
+} 
+
+int twoSum3(vector<int> &arr , int target ) {
+    int n = arr.size() ;
+
+    // making use of two pointers technique to find the solution \ 
+    sort(arr.begin() , arr.end() ) ; 
+
+    int left = 0 ; 
+    int right = n-1 ; 
+
+    while(left < right) {
+        int sum = arr[left] + arr[right] ; 
+
+        if( sum > target ){
+            right-- ;
+        } 
+        else if( sum < target) {
+            left++ ;
+        } 
+        else {
+            return true ;
+        }
+    } 
+    return false ;  
+} 
+
+int twoSum4(vector<int> &arr , int target){
+    int n = arr.size() ; 
+
+    // using an unordered_set to store the solution and check if its complement exists 
+    unordered_set<int> st ;  
+    for(int i=0 ; i<n ; i++){
+        int comp = target - arr[i] ; 
+
+        if(st.find(comp) != st.end() ) {
+            return true ;
+        } 
+        
+        st.insert(arr[i]) ;
+    } 
+
+    return false ; 
+}
 int main() {
     vector<int> arr = {0, -1, 2, -3, 1};
     int target = -2;
 
     if(two(arr, target))
+      cout << "true";
+    else
+      cout << "false";
+
+      
+    if(twoSum2(arr, target))
+      cout << "true";
+    else
+      cout << "false";
+
+      
+    if(twoSum3(arr, target))
+      cout << "true";
+    else
+      cout << "false";
+   
+    if(twoSum4(arr, target))
       cout << "true";
     else
       cout << "false";
