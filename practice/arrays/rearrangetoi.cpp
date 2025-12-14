db1f34338849e37d0ddde0ebc5055c3553d9799c
Prashant <prashantkumargouda17@gmail.com>
2025-12-14 22:13:22 +0530

    added some more files

---
+++ practice/arrays/rearrangetoi.cpp
@@ -0,0 +1,14 @@
+// Input: arr[] = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
+// Output: [-1, 1, 2, 3, 4, -1, 6, -1, -1, 9]
+// Explanation: In range 0 to 9, all except 0, 5, 7 and 8 are present. Hence, we print -1 instead of them.
+
+// Given an array of elements of length n, ranging from 0 to n - 1. All elements may not be present in the array. If the element is not present then there will be -1 present in the array. Rearrange the array such that arr[i] = i and if i is not present, display -1 at that place.  
+
+#include <iostream>
+#include <vector> 
+
+using namespace std ; 
+
+int main() {
+    
+}
