dd9f5d0c49c4e938d53d16c061ff7e15ff3e397a
Prashant <prashantkumargouda17@gmail.com>
2025-12-17 21:35:01 +0530

    added one

--- practice/arrays/rearrangetoi.cpp
+++ practice/arrays/rearrangetoi.cpp
@@ -1,14 +1,66 @@
-// Input: arr[] = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
-// Output: [-1, 1, 2, 3, 4, -1, 6, -1, -1, 9]
-// Explanation: In range 0 to 9, all except 0, 5, 7 and 8 are present. Hence, we print -1 instead of them.
-
-// Given an array of elements of length n, ranging from 0 to n - 1. All elements may not be present in the array. If the element is not present then there will be -1 present in the array. Rearrange the array such that arr[i] = i and if i is not present, display -1 at that place.  
-
-#include <iostream>
+#include <iostream>  
 #include <vector> 
+using namespace std ;  
 
-using namespace std ; 
+void modify(vector<int> &arr) {
+    int n = arr.size() ; 
 
-int main() {
+    for(int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<n ; j++) {
+            if( arr[j] == i ) {
+                swap( arr[i] , arr[j]) ; 
+                break ; 
+            }
+        }
+    } 
+
+    for(int i=0 ; i<n ; i++) {
+        if( arr[i] != i ) {
+            arr[i] = -1 ;
+        }
+    } 
     
+} 
+
+void modify2(vector<int> &arr) {
+    // using a temporary array 
+    int n = arr.size() ; 
+
+    vector<int> temp(n , -1) ; 
+
+    for(int i=0 ; i<n ; i++ ){
+        if( arr[i] != -1 ) {
+            temp[arr[i]] = arr[i] ; 
+        }  
+    }
+    for(int i=0 ; i<n ; i++) {
+        arr[i] = temp[i] ; 
+    } 
+
+} 
+
+void modify3(vector<int> &arr) {
+    int i=0 ; 
+
+    while( i<arr.size() ) {
+        if( arr[i] != -1 && arr[i] != arr[arr[i]]) {
+            swap(arr[i] , arr[arr[i]]) ;
+        }else {
+            i++ ;
+        }
+    }
+} 
+
+int main() { 
+    vector<int> arr = { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 }; 
+    // modify(arr) ; 
+    // modify2(arr) ;  
+    modify3(arr) ;
+    for(int x : arr) {
+        cout << x << " " ;  
+    } 
+
+    cout << endl; 
+
+    return 0 ; 
 }
