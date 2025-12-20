be87442d4e5dc317094e351721d1ee0631ea8976
Prashant <prashantkumargouda17@gmail.com>
2025-12-20 18:58:36 +0530

    added some more

--- practice/arrays/circularArrMaxSum.cpp
+++ practice/arrays/circularArrMaxSum.cpp
@@ -49,10 +49,46 @@ int maxSum2(vector<int> &arr) {
     return max(globalMax , total - globalMin) ; 
 }
 
+int maxSum3(vector<int> &arr) {
+    int n = arr.size() ; 
+
+    // trying to find the maxSuffix for an element  
+    int suffixSum = arr[n-1] ; 
+
+    vector<int> maxSuffix( n+1 , 0 ) ; 
+    maxSuffix[n-1] = arr[n-1] ; 
+
+    for(int i=0 ; i<n ; i++) {
+        suffixSum += arr[i]; 
+        maxSuffix[i] = max( maxSuffix[i+1] , suffixSum ) ; 
+    }   
+
+    // now the circularSum is the maximum circular Sum  
+    // and the normalSum is the maximum sum in the normal array 
+    int circularSum = arr[0] ; 
+    int normalSum  = arr[0] ; 
+
+    int prefix = 0 ; 
+    int currSum = 0 ;  
+
+    for(int i=0 ; i<n ; i++) {
+        currSum = max( currSum + arr[i] , arr[i] ); 
+        normalSum = max( normalSum , currSum ) ;  
+
+        prefix = prefix + arr[i] ; 
+        circularSum = max(circularSum , prefix + maxSuffix[i+1] ) ; 
+
+    } 
+
+    return max( circularSum , normalSum )  ;
+
+}
+
 
 int main() {
     vector<int> arr = {8, -8, 9, -9, 10, -11, 12} ; 
     cout << maxSum1(arr) << endl; 
-    cout << maxSum2(arr) << endl; 
+    cout << maxSum2(arr) << endl;  
+    cout << maxSum3(arr) << endl; 
     return 0 ; 
 }
