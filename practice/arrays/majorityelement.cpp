90f85f767320fdc3dc7541182ead87ed20ef5cea
Prashant <prashantkumargouda17@gmail.com>
2025-12-24 19:31:55 +0530

    added

--- practice/arrays/majorityelement.cpp
+++ practice/arrays/majorityelement.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <algorithm>
 #include <vector> 
+#include <unordered_map>  
 
 using namespace std ; 
 
-int findMajor(vector<int> &arr) {
+int findMajority0(vector<int> &arr) {
     int n = arr.size(); 
 
     for(int i=0 ; i<n ; i++){
@@ -23,7 +24,7 @@ int findMajor(vector<int> &arr) {
     return -1 ; 
 } 
 
-int findMajority(vector<int> &arr) {
+int findMajority1(vector<int> &arr) {
     int n = arr.size() ; 
 
     sort( arr.begin() , arr.end() ) ; 
@@ -39,16 +40,62 @@ int findMajority(vector<int> &arr) {
     return -1 ; 
 }  
 
-int findMajority(vector<int> &arr) {
+int findMajority2(vector<int> &arr) {
     int n = arr.size() ; 
 
-    
+    // okay so sorting out the elements and 
+    // uske baad use the 
+    // potentital majority element  
+
+    int count = 0 ; 
+
+    sort( arr.begin() , arr.end() ) ; 
+    int candidate = arr[n/2] ; 
+    for(int num: arr) { 
+        if( num == candidate )  {
+            count++ ;
+        }
+    }  
+
+    if( count > n/2 ) {
+        return candidate ; 
+    } 
+
+    return -1 ; 
+} 
+
+int findMajority3(vector<int> &arr) {
+    int n = arr.size() ; 
+
+    // using a hashmap 
+
+    unordered_map<int , int> countMap ;  
+
+    for(int x : arr) {
+        countMap[x]++ ; 
+
+        if( countMap[x] > n/2 ) {
+            return x ; 
+        }
+    } 
+
+    return -1 ; 
+} 
+
+int findMajority4(vector<int> &arr) {
+    int n = arr.size() ; 
+
+    // using moores voting algorithm  
+     
 }
 
 int main() {
     vector<int> arr = {1, 5, 2, 5, 3, 5, 5};   
 
-    cout << findMajor(arr) << endl; 
+    cout << findMajority0(arr) << endl; 
+    cout << findMajority1(arr) << endl; 
+    cout << findMajority2(arr) << endl;  
+    cout << findMajority3(arr) << endl; 
 
-    return 0 ;
+    return 0 ; 
 }
