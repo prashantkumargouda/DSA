df88f1523480f9efcc697de028ca2023be26a7fe
Prashant <prashantkumargouda17@gmail.com>
2025-12-24 13:52:33 +0530

    added one more

--- practice/arrays/smallestSubarraywithgreaterthanx.cpp
+++ practice/arrays/smallestSubarraywithgreaterthanx.cpp
@@ -58,15 +58,55 @@ int smallestSubArray2(vector<int> &arr , int x) {
 
     return res ;
 
-} 
+}  
+
+int smallestSubArray3(vector<int> &arr , int x ) {
+    int n = arr.size() ; 
+
+    int i=0 , j=0 , sum =0 ; 
+    int res = INT_MAX ; 
+
+    while( j < n ) {
+
+        // finding the sum until it is less than or equal to x   
+        // while j < n keep adding to make the sum  
+        // the moment it crosses it just stop 
+        // check if it j == arr.size() 
+        // if yes break the loop 
+        // else 
+        // start increasing the i value to change the window 
+        // now keep doing this and removing the values there is a window 
+        // and minimise the res or window
+
+        while( j < arr.size() && sum <= x ){
+            sum += arr[j++] ; 
+        } 
+
+        if( j == arr.size() && sum <= x ) break ;
+
+        while( i<j && sum - arr[i] > x ){
+            sum = sum - arr[i++] ; 
+        }  
+
+        res = min( res , j-i ) ;   
+        sum = sum - arr[i] ;
+        i++ ;
+    }  
+
+    if( res == INT_MAX ) {
+        return 0 ; 
+    } 
+
+    return res ; 
+
+    
+}
 
 int main(){
     vector<int> arr = {1, 4, 45, 6, 10, 19} ;  
 
     cout << smallestSubArray1(arr , 51) << endl; 
-    
     cout << smallestSubArray2( arr , 51 ) << endl;     
-    
-
+    cout << smallestSubArray3(arr , 51) << endl; 
     return 0 ; 
 } 
