916e253d767d571d818fa492409db70703f0e3aa
Prashant <prashantkumargouda17@gmail.com>
2025-12-24 18:23:11 +0530

    added one more

--- practice/arrays/majorityelement.cpp
+++ practice/arrays/majorityelement.cpp
@@ -1,8 +1,31 @@
-#include <iostream> 
+#include <iostream>
+#include <algorithm>
 #include <vector> 
 
 using namespace std ; 
 
+int findMajor(vector<int> &arr) {
+    int n = arr.size(); 
+
+    for(int i=0 ; i<n ; i++){
+        int count = 0 ; 
+        for(int j=0 ; j<n ; j++) {
+            if( arr[i] == arr[j] ) {
+                count++ ; 
+            }
+        } 
+
+        if( count > n/2 ) {
+            return arr[i] ;
+        }
+    } 
+
+    return -1 ; 
+}
 int main() {
-    
+    vector<int> arr = {1, 1, 2, 1, 3, 5, 1};   
+
+    cout << findMajor(arr) << endl; 
+
+    return 0 ;
 }
