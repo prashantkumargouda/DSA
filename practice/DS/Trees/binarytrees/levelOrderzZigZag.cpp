f0e749ed808c0a98d7a8d2f19c73dca2ed1799a2
Prashant <prashantkumargouda17@gmail.com>
2026-01-05 21:35:23 +0530

    added the correct one

--- practice/DS/Trees/binarytrees/levelOrderzZigZag.cpp
+++ practice/DS/Trees/binarytrees/levelOrderzZigZag.cpp
@@ -1,41 +1,77 @@
-// 103. Binary Tree Zigzag Level Order Traversal 
-// Given the root of a binary tree, return the zigzag level order traversal of its nodes' 
-// values. (i.e., from left to right, then right to left for the next level and alternate between). 
+#include <iostream> 
+#include <deque> 
+#include <vector> 
 
-// medium 
+using namespace std ; 
 
-class Solution {
-public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        if (!root) return res;
+class Node{
+public : 
+    int data ; 
+    Node* left ; 
+    Node* right ; 
 
-        deque<TreeNode*> dq;
-        dq.push_back(root);
-        bool reverse = false;
-
-        while (!dq.empty()) {
-            int size = dq.size();
-            vector<int> level;
-
-            for (int i = 0; i < size; i++) {
-                if (!reverse) {
-                    TreeNode* node = dq.front(); dq.pop_front();
-                    level.push_back(node->val);
-                    if (node->left) dq.push_back(node->left);
-                    if (node->right) dq.push_back(node->right);
-                } else {
-                    TreeNode* node = dq.back(); dq.pop_back();
-                    level.push_back(node->val);
-                    if (node->right) dq.push_front(node->right);
-                    if (node->left) dq.push_front(node->left);
-                }
-            }
-
-            res.push_back(level);
-            reverse = !reverse;
-        }
-
-        return res;
+    Node(int x){
+        data = x ; 
+        left = right = NULL ; 
     }
-};
+}; 
+
+vector<vector<int>> zigzag(Node* root) {
+    vector<vector<int>> res ; 
+    if( root == NULL ) return res; 
+
+    deque<Node*> dq ; 
+    dq.push_back(root) ;  
+
+    bool reverse = false ; 
+
+    while( !dq.empty() ) {
+        int size = dq.size() ; 
+        vector<int> level ; 
+
+        for(int i=0 ; i<size ; i++){
+            if( !reverse ) {
+                Node* curr = dq.front() ; 
+                dq.pop_front() ; 
+
+                level.push_back(curr -> data) ; 
+                if( curr -> left ) dq.push_back(curr -> left) ; 
+                if( curr -> right ) dq.push_back(curr -> right) ; 
+            } 
+            else {
+                Node* curr = dq.back() ; 
+                dq.pop_back() ; 
+
+                level.push_back(curr -> data) ; 
+                if( curr -> right ) dq.push_front( curr -> right) ; 
+                if( curr -> left ) dq.push_front(curr -> left) ; 
+            }
+        }  
+
+        res.push_back(level) ;
+        reverse = !reverse ; 
+    } 
+
+    return res ; 
+} 
+
+int main() {
+    Node* root = new Node(3) ;
+    root -> left = new Node(9) ;
+
+    root -> right = new Node(20) ; 
+    root -> right -> left = new Node(15) ; 
+
+    root -> right -> right = new Node(7) ; 
+
+    vector<vector<int>> temp = zigzag(root) ; 
+
+    for(auto &e : temp) {
+        for(int x:e) {
+            cout << x << " " ; 
+        }   
+        cout << endl; 
+    } 
+
+    return 0 ; 
+} 
