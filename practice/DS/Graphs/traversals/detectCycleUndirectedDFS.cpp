93e90051e3fdb9ed1931f7c16ce53c3b1ae946be
Prashant <prashantkumargouda17@gmail.com>
2026-02-03 20:32:11 +0530

    added

--- practice/DS/Graphs/traversals/detectCycleUndirectedDFS.cpp
+++ practice/DS/Graphs/traversals/detectCycleUndirectedDFS.cpp
@@ -7,7 +7,36 @@
 
 using namespace std ;
 
+bool dfs(int node , int parent , vector<int> adj[] , vector<int> &vis)  { 
+    vis[node] = 1 ; 
 
+    for(auto it : adj[node]) {
+        if( !vis[it] ) {
+            if(dfs(it , node , adj , vis)) {
+                return true ; 
+            } 
+            else if( it != parent ) {
+                return true ; 
+            }
+        }
+    } 
+
+    return false ; 
+} 
+
+bool isCycle(int V , vector<int> adj[]) {
+    vector<int> vis(V , 0) ; 
+
+    for(int i=0 ; i<V ; i++) {
+        if( !vis[i] ) {
+            if(dfs(i , -1 , adj , vis)) {
+                return true ; 
+            }
+        }
+    } 
+
+    return false ; 
+}
 
 int main() {
 
