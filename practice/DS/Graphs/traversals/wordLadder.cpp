4b519ed5e8eb5269371a98841d7f8b72c874bf41
Prashant <prashantkumargouda17@gmail.com>
2026-02-07 10:50:35 +0530

    added one more

--- practice/DS/Graphs/traversals/wordLadder.cpp
+++ practice/DS/Graphs/traversals/wordLadder.cpp
@@ -10,3 +10,46 @@
 // Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
 
 
+#include <iostream> 
+#include <queue> 
+#include <unordered_set> 
+
+using namespace std ; 
+
+int wordLenght(string beginWord , string endWord , vector<string> &wordList) {
+    queue<pair<string , int>> q ; 
+    q.push({beginWord , 1}) ; 
+
+    unordered_set<string> st( wordList.begin() , wordList.end() ) ; 
+
+    st.erase(beginWord) ; 
+
+    while(!q.empty()) {
+        string word = q.front().first ; 
+        int steps = q.front().second ; 
+
+        q.pop() ; 
+
+        if( word == endWord ) return steps ;  
+
+        for(int i=0 ; i<word.size() ; i++) {
+            char original = word[i] ; 
+
+            for(char ch = 'a' ;  ch <= 'z' ; ch++){
+                word[i] = ch ; 
+                if( st.find(word) != st.end() ) {
+                    st.erase(word) ; 
+                    q.push({word , steps+1}) ; 
+                }
+            } 
+
+            word[i] = original ; 
+        }
+    } 
+
+    return 0 ; 
+} 
+
+int main() {
+
+}
