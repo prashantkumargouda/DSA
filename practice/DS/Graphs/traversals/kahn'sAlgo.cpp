3dc05778176c41f88eef004bbb16402bef1305a0
Prashant <prashantkumargouda17@gmail.com>
2026-02-20 21:34:00 +0530

    added

--- practice/DS/Graphs/traversals/kahn'sAlgo.cpp
+++ practice/DS/Graphs/traversals/kahn'sAlgo.cpp
@@ -1,24 +1,58 @@
-// trying out topological sort using BFS 
 // Kahn's Algorithm 
+// topological sort using bfs 
 
-#include <iostream> 
-#include <queue> 
+#include <iostream>  
 #include <vector> 
+#include <queue> 
 
-using namespace std ;
- 
+using namespace std ; 
 
-vector<int> topoSort(int V , vector<vector<int>> &edges) {
+vector<int> kahns(int V , vector<vector<int>> &edges) { 
     vector<vector<int>> adj(V) ; 
-
     for(auto &e : edges) {
         adj[e[0]].push_back(e[1]) ; 
+    }  
+
+    // iske baad create the indegree vector 
+
+    vector<int> indegree(V , 0) ; 
+
+    for(int i=0 ; i<V ; i++ ){
+        for(auto it : adj[i]) {
+            indegree[it]++ ; 
+        }
+    }   
+
+    queue<int> q ; 
+
+    for(int i=0 ; i<V ; i++ ) {
+        if(indegree[i] == 0 ) {
+            q.push(i) ; 
+        }
     } 
 
-    vector<int> vis(V , 0) ; 
-    
+    vector<int> ans ; 
+    while( !q.empty() ) {
+        int node = q.front() ; 
+        q.pop() ; 
+        ans.push_back(node) ;
+
+        // okay so now 
+        // as I remove the elements from the queue I add those into the ans 
+        // visit the adjacent nodes 
+
+        for(auto it : adj[node]) {
+            indegree[it]-- ; 
+            if(indegree[it] == 0){
+                q.push(it) ; 
+            }
+        } 
+    }
+
+    return ans ; 
+
 } 
 
-int main(){
+int main() {
 
 }
--- practice/DS/Graphs/traversals/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-if( find(terminal.begin() , terminal.end() , node) != terminal.end() ) { 
-        return true ;  
-    }
