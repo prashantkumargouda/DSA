5d462082758c41dc4f288aaea5a21a4db8b5f5f7
Prashant <prashantkumargouda17@gmail.com>
2026-02-21 11:18:08 +0530

    a

---
+++ practice/DS/Graphs/traversals/detectCycleinGraphBFS.cpp
@@ -0,0 +1,58 @@
+#include <iostream>  
+#include <vector> 
+#include <queue> 
+
+using namespace std ; 
+
+bool kahns(int V , vector<vector<int>> &edges) { 
+    vector<vector<int>> adj(V) ; 
+    for(auto &e : edges) {
+        adj[e[0]].push_back(e[1]) ; 
+    }  
+
+    // iske baad create the indegree vector 
+
+    vector<int> indegree(V , 0) ; 
+
+    for(int i=0 ; i<V ; i++ ){
+        for(auto it : adj[i]) {
+            indegree[it]++ ; 
+        }
+    }   
+
+    queue<int> q ; 
+
+    for(int i=0 ; i<V ; i++ ) {
+        if(indegree[i] == 0 ) {
+            q.push(i) ; 
+        }
+    } 
+
+    int count = 0 ; 
+
+    vector<int> ans ; 
+    while( !q.empty() ) {
+        int node = q.front() ; 
+        q.pop() ; 
+        count++ ;
+
+        // okay so now 
+        // as I remove the elements from the queue I add those into the ans 
+        // visit the adjacent nodes 
+
+        for(auto it : adj[node]) {
+            indegree[it]-- ; 
+            if(indegree[it] == 0){
+                q.push(it) ; 
+            }
+        } 
+    }
+    if( ans.size() != V ) return true ;
+
+    return false ; 
+
+} 
+
+int main() {
+
+}
