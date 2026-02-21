748272505207be053dbb3adb3294f12da48fb5e5
Prashant <prashantkumargouda17@gmail.com>
2026-02-21 08:42:54 +0530

    added

---
+++ practice/DS/Graphs/traversals/courseSchedule.cpp
@@ -0,0 +1,24 @@
+// 207. Course Schedule
+// Medium
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false. 
+
+#include <iostream>  
+#include <vector> 
+
+using namespace std ; 
+
+bool canFinish(int numCourses , vector<vector<int>> &preRequisites) {
+    // provided the number of courses 
+    // What I need to do is ? find if taking those courses is possible or not 
+    // if there is a cycle it is not possible 
+
+    
+} 
+
+int main() {
+    
+}
