549d5123ea8f2ce2b50f04183e544e12c64d22fe
Prashant <prashantkumargouda17@gmail.com>
2026-02-23 21:39:50 +0530

    added

---
+++ practice/DS/Graphs/traversals/djikstra'sAlgo.cpp
@@ -0,0 +1,22 @@
+// using the djisktras algorithm  
+// to find the minimum cost distance from the source node to target node 
+
+// given a weighted undirected graph and a source vertex src. 
+// We need to find the shortest path distances from the source vertex to all other vertices in the graph. 
+
+// Input: src = 0, adj[][] = [[[1, 4], [2, 8]],         
+//                                             [[0, 4], [4, 6], [2,3]], 
+//                                             [[0, 8], [3, 2], [1,3]], 
+//                                             [[2, 2], [4, 10]], 
+//                                             [[1, 6], [3, 10]]] 
+
+#include <iostream>  
+#include <vector> 
+
+using namespace std ; 
+
+
+int main() 
+{
+
+}
