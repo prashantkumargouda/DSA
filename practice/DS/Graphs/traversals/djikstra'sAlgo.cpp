231f8387458e4c3ff63f0ea77fa11adc57f2348d
Prashant <prashantkumargouda17@gmail.com>
2026-02-26 18:59:33 +0530

    added

--- practice/DS/Graphs/traversals/djikstra'sAlgo.cpp
+++ practice/DS/Graphs/traversals/djikstra'sAlgo.cpp
@@ -10,8 +10,50 @@
 //                                             [[2, 2], [4, 10]], 
 //                                             [[1, 6], [3, 10]]] 
 
-#include <iostream> 
+#include <iostream>  
+#include <vector> 
+#include <queue> 
+
 using namespace std ; 
+
+vector<int> findShortest(int V , vector<vector<int>> &edges , int src){
+    vector<vector<pair<int, int>>> adj(V) ; 
+    vector<int> dis(V) ; 
+
+    for(auto &e : edges) {
+        int u = e[0] ; 
+        int v = e[1] ; 
+        int wt = e[2] ;
+         
+        adj[u].push_back({v , wt}) ;
+        adj[v].push_back({u , wt}) ;
+    }  
+
+    priority_queue< pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq ; 
+    
+    dis[src] = 0 ; 
+    pq.push({0 , src}) ; 
+
+    while( !pq.empty() ) {
+        int node = pq.top().second ; 
+        int dist = pq.top().first ; 
+
+        pq.pop() ; 
+        if( dist > dis[node] ) continue ;  
+
+        for(auto &it : adj[node]) {
+            int v = it.first ; 
+            int wt = it.second ; 
+
+            if( dist + wt < dis[v] ) {
+                dis[v] = dist + wt ;  
+                pq.push({ dis[v] , v }) ; 
+            } 
+        }
+    } 
+
+    return dis ; 
+} 
 
 int main() { 
 
