f4210f72260496be2ee759d568490aa79e24913a
Prashant <prashantkumargouda17@gmail.com>
2026-03-21 09:20:37 +0530

    changed one more

--- practice/DS/arrays/striverssheetarray/setMatrixZero.cpp
+++ practice/DS/arrays/striverssheetarray/setMatrixZero.cpp
@@ -1,42 +1,63 @@
-// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's. 
+#include <iostream>
+#include <vector> 
 
-#include <iostream>  
-#include <vector>
+using namespace std ;  
 
-using namespace std ; 
+void solve1(vector<vector<int>> &mat){ 
+    int n = mat.size() ; 
+    int m = mat[0].size() ; 
 
-void solve(vector<vector<int>> &matrix) {
-    int m = matrix.size() ; 
-    int n = matrix[0].size() ; 
+    int col0 = 1; 
 
-    vector<int> col(n , 0) ; 
-    vector<int> row(m , 0) ; 
-
-    for(int i=0 ; i<m ; i++) {
-        for( int j=0 ; j<n ; j++) {
-            if( matrix[i][j] == 0 ) {
-                col[j] = 1 ; 
-                row[i] = 1 ; 
+    for(int i=0 ; i<n ;i++) {
+        for(int j=0 ; j<m ; j++){
+            if( mat[i][j] == 0 ) {
+                mat[i][0] = 0 ; 
+                if( j!= 0 ) {
+                    mat[0][j] = 0 ; 
+                }else{
+                    col0 = 0 ; 
+                }
             }
         }
     } 
 
+    for(int i=1 ; i<n ; i++ ){
+        for(int j=1 ; j<m ; j++) {
+            if( mat[i][j] != 0 ) {
+                if( mat[i][0] == 0 || mat[0][j] == 0 ) {
+                    mat[i][j] = 0 ; 
+                }
+            }
+        }
+    } 
 
-    for(int i=0 ; i<m ; i++) {
-        for(int j=0 ; j<n ; j++) {
-            if( col[j] == 1 || col[i] == 1 ){
-                matrix[i][j] = 0 ;
+    if( mat[0][0] == 0 ) {
+        for(int j=1 ; j<m ; j++){
+            if( mat[0][j] != 0 ) {
+                mat[0][j] = 0 ; 
             }
         }
     }  
-} 
+
+    if( col0 == 0 ) {
+        for(int i=0 ; i<n ; i++) {
+            mat[i][0] = 0 ; 
+        }
+    }
+}   
 
 int main() {
-    int m,n ;  
-    cin >> m >> n ; 
+    int n , m ; 
+    cin >> n >> m ; 
 
-    vector<vector<int>> matrix( m , vector<int>(n) ) ; 
+    vector<vector<int>> mat(n , vector<int>(m))  ; 
 
-    
-    
+    for(int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<m ; j++) {
+            cin >> mat[i][j] ; 
+        }
+    } 
+
+    return 0 ; 
 }
