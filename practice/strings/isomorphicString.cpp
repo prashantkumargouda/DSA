b5b54107da7fef0059ccdb3ad2c99d2da3d12a7a
Prashant <prashantkumargouda17@gmail.com>
2026-03-30 18:45:16 +0530

    added

--- practice/strings/isomorphicString.cpp
+++ practice/strings/isomorphicString.cpp
@@ -1,3 +1,17 @@
 // isomorphic string 
 // Problem Statement: Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself. 
+
+#include <iostream> 
+#include <string> 
+#include <vector> 
+
+using namespace std ; 
+
+int main() {
+    string s , t  ; 
+    cin >> s >> t ; 
+
+    
+    return 0 ; 
+}
