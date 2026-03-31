1897e239e32f55d52cd193ca5a98d07ef13e4eff
Prashant <prashantkumargouda17@gmail.com>
2026-03-31 20:41:40 +0530

    added

--- contests/codeforces/900/shiftedMex.cpp
+++ contests/codeforces/900/shiftedMex.cpp
@@ -1,15 +1,52 @@
-#include <iostream>  
+#include <iostream> 
 #include <vector> 
+#include <algorithm> 
+#include <climits> 
+#include <set> 
 
 using namespace std ; 
 
-int main() {
-    int n ; 
-    cin >> n ; 
+int compute(vector<int> &nums ,int shift) {
+    int n = nums.size() ; 
 
-    vector<int> nums(n) ; 
-    for(auto &it : nums) cin >> it ; 
+    set<int> s ; 
+    for(int num : nums) {
+        s.insert( num + shift ) ;  
+    } 
 
+    int mex = 0 ; 
+    while( s.count(mex) ) { 
+        mex++ ; 
+    } 
+
+    return mex ; 
+
+} 
+
+int check(vector<int> &nums) {
+    int n = nums.size() ; 
     
-    return 0 ; 
+    int ans = 0 ; 
+    for(int num : nums) {
+        int shift = -num ; 
+        ans = max( ans , compute(nums , shift)) ; 
+    } 
+    
+    return ans ; 
+}
+
+int main() { 
+    int t ; 
+    cin >> t ; 
+    while(t--) {
+        int n ; 
+        cin >> n ; 
+
+        vector<int> nums(n);
+        for(auto &it : nums) cin >> it ; 
+
+        cout << check(nums) << endl;  
+    }
+
+    return 0 ;  
 }
